9ece57248a13244fae68cbea7c841936c63666aa
Luke Zeches <zechesl@gmail.com>
2024-01-27 21:40:29 -0500

    Moved boss damage to function

--- Assets/Scripts/BossManager.cs
+++ Assets/Scripts/BossManager.cs
@@ -10,46 +10,15 @@ public class BossManager : MonoBehaviour
         RANGED = 1
     }
     public static Transform player;
-    public static bool allowMove;
+    public static bool allowMove, isStunned;
     public static BossState state;
-    private int stage; // Start at stage 0 (ranged)
-    private float health;
+    private static int stage; // Start at stage 0 (ranged)
+    private static float health;
     [SerializeField]
-    private float maxHealth;
-    public float Health
-    {
-        get { return health; }
-        set
-        {
-            health = value;
-            if (stage == 0 && health <= maxHealth * 0.75f)
-            {
-                // Move to stage 1 (melee)
-                allowMove = true;
-                state = BossState.MELEE;
-                stage = 1;
-            }
-            else if (stage == 1 && health <= maxHealth * 0.5f)
-            {
-                // Move to stage 2 (ranged)
-                allowMove = false;
-                state = BossState.RANGED;
-                stage = 2;
-            }
-            else if (stage == 2 && health <= maxHealth * 0.25f)
-            {
-                // Move to stage 3 (melee)
-                allowMove = true;
-                state = BossState.MELEE;
-                stage = 3;
-            }
-            else if (health <= 0)
-            {
-                health = 0;
-                BossDeath();
-            }
-        }
-    }
+    private static float maxHealth;
+    private float stunTimer;
+    [SerializeField]
+    private float stunDuration;
 
     private void Start()
     {
@@ -59,7 +28,15 @@ public class BossManager : MonoBehaviour
         state = BossState.RANGED;
         stage = 0; // Start at stage 0 (ranged)
     }
-    private void SwitchState()
+    private void Update()
+    {
+        if (isStunned)
+        {
+            if (stunTimer <= 0) isStunned = false;
+            else stunTimer -= Time.deltaTime;
+        }
+    }
+    private static void SwitchState()
     {
         if (state == BossState.MELEE)
         {
@@ -70,9 +47,39 @@ public class BossManager : MonoBehaviour
             state = BossState.MELEE;
         }
     }
-    private void BossDeath()
+    private static void BossDeath()
     {
         allowMove = false;
         Debug.Log("Boss dead");
+    }
+    public static void DamageBoss()
+    {
+        health -= 1;
+        if (stage == 0 && health <= maxHealth * 0.75f)
+        {
+            // Move to stage 1 (melee)
+            allowMove = true;
+            state = BossState.MELEE;
+            stage = 1;
+        }
+        else if (stage == 1 && health <= maxHealth * 0.5f)
+        {
+            // Move to stage 2 (ranged)
+            allowMove = false;
+            state = BossState.RANGED;
+            stage = 2;
+        }
+        else if (stage == 2 && health <= maxHealth * 0.25f)
+        {
+            // Move to stage 3 (melee)
+            allowMove = true;
+            state = BossState.MELEE;
+            stage = 3;
+        }
+        else if (health <= 0)
+        {
+            health = 0;
+            BossDeath();
+        }
     }
 }
