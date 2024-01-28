798bbf7c041050456237248707c837fd92cdf167
yoshi-the-drunk <mrrpiano02@gmail.com>
2024-01-28 14:13:02 -0500

    Parallax!!!

--- Assets/Scripts/BGScroll.cs
+++ Assets/Scripts/BGScroll.cs
@@ -7,6 +7,7 @@ public class BGScroll : MonoBehaviour
     Transform player;
     public float offset = 5.0f;
     public float distFromOffset;
+    public float scrMultiplier; // 0 to 1
     float limit = 25.0f;
 
     // Start is called before the first frame update
@@ -20,15 +21,15 @@ public class BGScroll : MonoBehaviour
     {
         if (player.position.z < (limit - distFromOffset) && player.position.z > (-limit + distFromOffset))
         {
-            if (player.position.z > offset)
+            if (player.position.z > (offset / scrMultiplier))
             {
                 Debug.Log("Scroll left.");
-                transform.position = new Vector3(transform.position.x, transform.position.y, -player.position.z + offset + distFromOffset);
+                transform.position = new Vector3(transform.position.x, transform.position.y, (-player.position.z * scrMultiplier) + offset - distFromOffset);
             }
-            else if (player.position.z < -offset)
+            else if (player.position.z < (offset / scrMultiplier))
             {
                 Debug.Log("Scroll right.");
-                transform.position = new Vector3(transform.position.x, transform.position.y, -player.position.z - offset + distFromOffset);
+                transform.position = new Vector3(transform.position.x, transform.position.y, (-player.position.z * scrMultiplier) + offset - distFromOffset);
             }
         }
         else Debug.Log("Stop scrolling.");
