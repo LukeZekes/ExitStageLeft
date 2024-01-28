b3d13fbd38f8416590906b7c1b7fd84838cf20d8
Luke Zeches <zechesl@gmail.com>
2024-01-27 22:29:04 -0500

    Remove damage testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,8 +6,4 @@ public class GameManager : MonoBehaviour
 {
     public PlayerManager player;
     public BossManager boss;
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) PlayerManager.TakeDamage();
-    }
 }
